ddbbb040c5e5c46b4674b4e37e27122a417db143
Ivette Nobiltá <ivette.nobilta@hotmail.com>
2024-05-03 12:32:11 -0300

    final adjustments

--- prueba/src/app/app.component.ts
+++ prueba/src/app/app.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent],
+  imports: [HomeComponent, RouterModule],
   template: `
   <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="././assets/descarga.jpg" alt="logo" aria-hidden="true">
-    </header>
+    <a [routerLink]="['/']">
+      <header class="brand-name">
+        <img class="brand-logo" src="././assets/descarga.jpg" alt="logo" aria-hidden="true" width="100px">
+        <h3>HOUSING</h3>
+      </header>
+    </a>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
 `,
